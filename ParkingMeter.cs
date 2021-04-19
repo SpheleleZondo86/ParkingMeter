d738ac05e121ac4cadab6ebc4b3aae327e7816fb
SpheleleZondo86 <sphelelezondo@yahoo.com>
2021-04-19 11:18:26 +0200

    Add project files.

---
+++ ParkingMeter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter
+{
+	public class ParkingMeter
+	{
+		public static void Main()
+		{
+			decimal amount = 109.34m, deduction = 23.33m;
+			Console.WriteLine(ChargeForParking(amount, deduction));
+			Console.Read();
+		}
+
+		public static string ChargeForParking(decimal amount, decimal deduction)
+		{
+			if (amount < deduction)
+				return $"Please put in an amount of {deduction}, or more!";
+
+			decimal change = amount - deduction;
+
+			return $"Your change is: R{change}";
+		}
+
+	}
+}
